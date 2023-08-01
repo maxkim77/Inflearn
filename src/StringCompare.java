defe7d16cf936ba96b5e1c44d2ddbf8a853891fb
maxkim77 <maxkim7723@gmail.com>
2023-08-02 00:00:00 +0900

    [Update] Commit

--- src/StringCompare.java
+++ src/StringCompare.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class StringCompare {
+import java.util.Scanner;
+public class StringCompare {
+    public static void main(String[] args) {
+        String a = "치킨";
+        Scanner scanner = new Scanner(System.in);
+        String b= scanner.nextLine();
+        System.out.println(a.equals(b));
+    }
 }
