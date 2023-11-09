4fd1d87c08a5734fe92be1069c55295928b8e1f1
maxkim77 <maxkim7723@gmail.com>
2023-11-09 20:48:42 +0900

    [add] update

---
+++ src/StringExample1.java
@@ -0,0 +1,7 @@
+public class StringExample1 {
+    public static void main(String[] args) {
+        String me = "살은 내가 쪄요.";
+        int length = me.length();
+        System.out.println(length);
+    }
+}
